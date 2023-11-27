e598b6185a8b443f8289b3d5f11f41fec8d57454
neerajsharma444 <neeraj999456@gmail.com>
2023-11-27 18:19:47 +0530

    Added a ternary on Stack>navigator

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -3,23 +3,27 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Register from '../screens/Register';
 import Login from '../screens/Login';
 import Home from '../screens/Home';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
-  const myApp = useDispatch(state => {
-    state.userReducer.loginUser;
-  });
+  const myApp = useSelector(state => state.userReducer.loginUser);
+  console.log('App Navigation', myApp);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
+      {myApp ? (
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Register" component={Register} />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 };
-
 export default AppNavigator;
